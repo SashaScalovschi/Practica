b67468427830bd14f0cc762f06840367f5e717b6
Aleksandryi <skalovskyi.oleksandr.clg@chnu.edu.ua>
2022-06-19 16:15:49 +0300

    da

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,10 +4,15 @@ export default function Footer(){
     return(
         <div className="footer">
             <p>Konst @2022</p>
+
             <div className="social">
-                <i className="fa fa-facebook"></i>
+                <i className="fa fa-facebook">s</i>
                 <i className="fa fa-instagram"></i>
             </div>
         </div>
+
+        
+
+        
     )
 }
